68d849e0fa9d3f2eab0d7a0db929351f0b3067a2
Mihai Lupu <mihailupu.dev@gmail.com>
2024-11-19 23:15:40 +0200

    Complete exercise

--- cap_2/grade/grade.go
+++ cap_2/grade/grade.go
@@ -7,14 +7,28 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Print("Enter a grade:")
+	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(input)
+
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var status string
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println("A grade of", grade, "is", status)
 }
